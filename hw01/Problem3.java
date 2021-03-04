ada4fc36fcd56f96fd991e96e8b279091ab057ce
Amandapoor <47338117+Amandapoor@users.noreply.github.com>
2021-03-03 21:11:42 -0500

    Add files via upload

---
+++ hw01/Problem3.java
@@ -0,0 +1,21 @@
+//Amanda Poor
+//Prof. Arias
+//Software Development 1
+
+// I will write a program that displays a big sign for 100 as shown in the example
+
+
+public class Problem3 {
+
+    public static void main(String[] args) {
+
+        //displays the pattern 
+        System.out.println(" *  *****  *****");
+        System.out.println(" *  *   *  *   *");
+        System.out.println(" *  *   *  *   *");
+        System.out.println(" *  *   *  *   *");
+        System.out.println(" *  *****  *****");
+
+    }
+}
+        
